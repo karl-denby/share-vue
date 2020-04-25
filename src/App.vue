407ddbe354c2806715cc9363b18361c034762d3b
Karl Denby <karl.denby@gmail.com>
2020-04-25 10:11:49 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <WhatDayIsIt v-bind:day=day />
+  </div>
+</template>
+
+<script>
+import WhatDayIsIt from './components/WhatDayIsIt.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WhatDayIsIt
+  },
+  data () {
+    return {
+      day: null
+    }
+  },
+  mounted () {
+    this.getDay();
+  },
+  methods: {
+    async getDay() {
+      const api_url ="https://api.karldenby.com/dayofweek";
+      console.log(`Using the url: ${api_url}`)
+      const res = await fetch(api_url)
+      const data = await res.json();
+      console.log(`Data returned is ${data.day}`)
+      this.day = data.day;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WhatDayIsIt.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="day">
+    <h1>{{ day }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WhatDayIsIt',
+  props: {
+    day: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
